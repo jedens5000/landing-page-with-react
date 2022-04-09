d375c33ef8b3a400d6b656cb5ee55f015f1e8719
jedens5000 <mrjonedens@gmail.com>
2022-04-09 13:35:56 +0000

    Fixed card display & layout

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Card = () => {
 	return (
-		<div className="row">
-			<div className="card" style="width: 18rem;">
+		<div className="row d-flex justify-content-center">
+			<div className="card m-5" style={{ width: "18rem" }}>
 				<img
 					src="https://picsum.photos/500/325"
 					className="card-img-top"
@@ -22,7 +22,7 @@ const Card = () => {
 					</a>
 				</div>
 			</div>
-			<div className="card" style="width: 18rem;">
+			<div className="card m-5" style={{ width: "18rem" }}>
 				<img
 					src="https://picsum.photos/500/325"
 					className="card-img-top"
@@ -41,7 +41,26 @@ const Card = () => {
 					</a>
 				</div>
 			</div>
-			<div className="card" style="width: 18rem;">
+			<div className="card m-5" style={{ width: "18rem" }}>
+				<img
+					src="https://picsum.photos/500/325"
+					className="card-img-top"
+					alt="card image"
+				/>
+				<div className="card-body text-center">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Praesent quis elit at ipsum iaculis congue. Quisque
+						hendrerit, purus et scelerisque faucibus, mauris augue
+						fermentum nibh, nec laoreet arcu leo vel erat.
+					</p>
+					<a href="#" className="btn btn-primary">
+						Find Out More!
+					</a>
+				</div>
+			</div>
+			<div className="card m-5" style={{ width: "18rem" }}>
 				<img
 					src="https://picsum.photos/500/325"
 					className="card-img-top"
